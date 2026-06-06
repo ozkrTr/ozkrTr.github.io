021d46356173cd8955c33b3067c5dfcb26406d6d
ozkrTr <torres3094@gmail.com>
2026-06-06 16:30:54 -0600

    Refactorización de CSS, transiciones, enfoque de formulario y Grid dinámico

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// ==========================================================
-// NUEVO SELECTOR: Al inicio de tu archivo
-// ==========================================================
-// 1. Ve a tu index.html, busca la sección de contacto y agrega un <div> vacío 
-//    abajo del formulario con el id="contenedorMensajes".
-// 2. Aquí en app.js, crea la constante para seleccionar ese nuevo contenedor por su ID.
-
-
-
-
-// ==========================================================
-// COMPORTAMIENTO AUTOMÁTICO (Efecto de Persistencia)
-// ==========================================================
-// 1. AL CARGAR LA PÁGINA: Ejecuta la función 'mostrarMensajes()' al final de tu archivo 
-//    (fuera de todo) para que pinte los datos que recuperó el LocalStorage al abrir el sitio.
-//
-// 2. AL ENVIAR EL FORMULARIO: Dentro de tu evento submit (en el bloque de ÉXITO),
-//    justo después de guardar en LocalStorage, llama también a 'mostrarMensajes()' 
-//    para que la pantalla se actualice en tiempo real sin tener que recargar.
 const formularioContacto = document.getElementById("formularioContacto");
 const inputNombre = document.getElementById("txtName");
 const inputEmail = document.getElementById("txtEmail");
@@ -38,24 +19,11 @@ function validarCampos(nombre, email) {
   }
 }
 
-// ==========================================================
-// NUEVA FUNCIÓN: Al final de tu archivo (Responsabilidad Única)
-// ==========================================================
-// Crea una función llamada 'mostrarMensajes' (no necesita recibir parámetros):
-// REGLAS DENTRO DE LA FUNCIÓN:
-// -> A. Limpia el contenedor HTML para que no se dupliquen los datos antiguos:
-//       (Asigna contenedorMensajes.innerHTML = "";)
-// -> B. Usa el método .forEach() para recorrer 'listaMensajes'.
-// -> C. Por cada 'mensaje' dentro del bucle:
-//       - Suma contenido a contenedorMensajes.innerHTML usando el operador '+='.
-//       - Inserta una estructura maquetada con Template Literals (comillas invertidas ``).
-//         Ejemplo: `<p><strong>${mensaje.nombre}</strong> (${mensaje.email})</p>`
-
 function mostrarMensajes() {
     contenedorMensajes.innerHTML = "";
 
     listaMensajes.forEach(mensaje => {
-        contenedorMensajes.innerHTML += `<p class="form__message"><strong>Nombre: ${mensaje.nombre}</strong>, Email: ${mensaje.email}</p>`;   
+        contenedorMensajes.innerHTML += `<p class="form__message"><strong>Nombre: </strong> ${mensaje.nombre} | <strong>Email: </strong>${mensaje.email}</p>`;   
     });
 }
 
